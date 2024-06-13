d50c39ae3ff2fc8c9d4fef17ff3f3fdf3ff3fee7
NhanPham <PC@DESKTOP-HR11HUG>
2024-06-13 22:59:50 +0700

    solve problem

--- src/java/DBContext/DBContext.java
+++ src/java/DBContext/DBContext.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DBContext;
 
-/**
- *
- * @author Admin
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DBContext {
-    
+
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber;
+        if (instance != null && !instance.trim().isEmpty()) {
+            url += "\\" + instance;
+        }
+        url += ";databaseName=" + dbName + ";encrypt=true;trustServerCertificate=true;sslProtocol=TLSv1.2";
+
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+
+    private final String serverName = "DESKTOP-HR11HUG";
+    private final String dbName = "BadmintonDB";
+    private final String portNumber = "1433";
+    private final String instance = "";
+    private final String userID = "sa";
+    private final String password = "123";
+
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+
+        try (Connection conn = db.getConnection()) {
+            System.out.println("Connected");
+        } catch (Exception ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
