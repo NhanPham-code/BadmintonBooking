a39b0a8981a5e684d581ed16483efdba9943aa44
ADMIN <ADMIN@LAPTOP-NEFQCNRS>
2024-07-02 13:35:07 +0700

    Stadium Management

--- src/java/model/Admin.java
+++ src/java/model/Admin.java
@@ -9,10 +9,22 @@ package model;
  * @author nhanPH
  */
 public class Admin {
+
     private String admin_ID;
     private String admin_name;
     private String admin_phone;
     private String acc_ID;
+
+    public Admin() {
+    }
+
+    
+    public Admin(String admin_ID, String admin_name, String admin_phone, String acc_ID) {
+        this.admin_ID = admin_ID;
+        this.admin_name = admin_name;
+        this.admin_phone = admin_phone;
+        this.acc_ID = acc_ID;
+    }
 
     public String getAdmin_ID() {
         return admin_ID;
@@ -45,7 +57,5 @@ public class Admin {
     public void setAcc_ID(String acc_ID) {
         this.acc_ID = acc_ID;
     }
-    
-    
 
 }
